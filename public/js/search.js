95914943286d6df0eb6a85f58b83ad99118f2ae2
m-huff <michaelhuff17@gmail.com>
2018-04-03 13:03:28 -0700

    Add search page, unfinished

--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 	$.get("/userInfo", success);
+	$.get("/findItems", {keywords:retID(window.location.href)}, function(data){
+		for(var item in data.items)
+			var divCreator = "<div class='itemBox'>
+								<img id='itemBoxPic' src=''>" + item.icon + "</img>
+								<label id='itemBoxName'>" + item.name + "</label>
+								<label id='itemBoxPrice'>" + item.price + "</label>
+				  			  </div>";
+			$(".searchHolder").append(divCreator);
+	});
 	$("#logout").click(() => {
 		$.post("/logout", (data) => {
 			window.location = window.location.href.split("/")[1] + data.redirect;
@@ -28,4 +37,11 @@ function success(data)
 	}
 	$("#userGreeting").html("Hello " + data.user.username + "!");
 	$("#password").html(data.user.password);
+}
+function retID(WINDOWURL)
+{
+	var rightSide = WINDOWURL.split("?")[1];
+	var findIt = rightSide.split("id")[1];
+	var maybe = findIt.split("=")[1];
+	return maybe;
 }
