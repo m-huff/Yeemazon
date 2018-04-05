4dccfe17e164fdf064e8d1b0ea3069761d7a3a46
rcbrian81 <rcbriansanchez@gmail.com>
2018-04-05 12:32:33 -0700

    mongoose

---
+++ models/product.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+let productSchema = mongoose.Schema({
+	name: {
+		type: String,
+		require: true
+	},
+	description: {
+		type: String,
+		require: true
+	},
+	price: {
+		type: String,
+		require: true
+	},
+	category: {
+		type: String,
+		require: true
+	},
+	keywords: [{
+		type: String
+	}]
+});
+
+let Poduct = module.exports = mongoose.model('Product',productSchema);
