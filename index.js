6fae66db8072fddbca4207d2bb5ff582dc98e377
VincentCosta6 <37250561+VincentCosta6@users.noreply.github.com>
2018-04-08 10:40:47 -0700

    Added HTTPS protection, only works for my ip sorry.

--- index.js
+++ index.js
@@ -25,6 +25,32 @@ app.use('/js', express.static('./public/js'));
 app.use('/images', express.static('./public/images'));
 app.use(routes);
 
+if(startup.https === true)
+{
+	var http = require('http');
+	var https = require('https');
 
-app.listen(startup.port);
-console.log("\n   Yeemazon server has initialized! \n      IP: " + myIP.address() + " Port: " + startup.port + "\n");
+	var pem = require('pem');
+	pem.config({
+ 	 pathOpenSSL: 'C:\\OpenSSL-Win32\\bin\\openssl'
+	})
+	var ret = pem.createCertificate({days:365, selfSigned:true}, function(err, keys){
+		if(err) throw err;
+		console.log(keys);
+		return keys;
+	});
+
+	var cred = {key : ret.serviceKey, cert : ret.certificate};
+
+	var httpServer = http.createServer(app);
+	var httpsServer = https.createServer(cred, app);
+	httpServer.listen(startup.port);
+	httpsServer.listen(startup.httpsPort);
+	console.log("\n   Yeemazon server has initialized! \n      IP: " + myIP.address() + " HTTP Port: " + startup.port + "\n      HTTPS Port: " + startup.httpsPort);
+}
+else
+{
+	app.listen(startup.port);
+	console.log("\n   Yeemazon server has initialized! \n      IP: " + myIP.address() + " Port: " + startup.port + "\n      HTTPS Port: DISABLED");
+}
+
